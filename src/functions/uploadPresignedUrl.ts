6a0e205b0cac6e55c867401ad29eca6b1972abbb
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2025-01-27 23:46:32 +0100

    Change uploadPresignedUrl flow to also include uploading temporary metadata to DynamoDB with TTL

--- src/functions/uploadPresignedUrl.ts
+++ src/functions/uploadPresignedUrl.ts
@@ -1,13 +1,18 @@
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-import { Handler } from "aws-lambda";
+import { APIGatewayProxyEvent, Handler } from "aws-lambda";
 import { v4 as uuidv4 } from 'uuid';
 
 const s3 = new S3Client({ region: process.env.SERVERLESS_AWS_REGION });
+const ddb = new DynamoDBClient({ region: process.env.SERVERLESS_AWS_REGION });
+const docClient = DynamoDBDocumentClient.from(ddb);
 
-export const handler: Handler = async (event) => {
+export const handler: Handler = async (event: APIGatewayProxyEvent) => {
     try {
-        const { fileName, fileType } = JSON.parse(event.body);
+        const { username, sub: userId } = event.requestContext.authorizer!.jwt.claims;
+        const { fileName, fileType, imageTitle, imageDescription } = JSON.parse(event.body ?? "{}");
         if (!fileName || !fileType) {
             return {
                 statusCode: 400,
@@ -15,20 +20,36 @@ export const handler: Handler = async (event) => {
             };
         }
 
-        const fileKey = `uploads/${uuidv4()}-${fileName}`;
+        const imageId = uuidv4() + "-" + fileName;
 
         const command = new PutObjectCommand({
             Bucket: process.env.IMAGES_BUCKET_NAME,
-            Key: fileKey,
+            Key: `uploads/${imageId}`,
             ContentType: fileType
         });
+
+        const imageMetadata: ImageMetadata = {
+            userId,
+            authorUsername: username,
+            imageTitle,
+            imageDescription,
+            imageId,
+            uploadedAt: new Date().toISOString(),
+            ttl: Math.floor(Date.now() / 1000) + 300
+        };
+
+        // Save the image metadata to DynamoDB (with 5 minutes TTL)
+        await docClient.send(new PutCommand({
+            TableName: process.env.IMAGES_TABLE_NAME,
+            Item: imageMetadata
+        }));
 
         // Generate a pre-signed URL for the S3 PUT operation (5 minutes)
         const url = await getSignedUrl(s3, command, { expiresIn: 300 });
 
         return {
             statusCode: 200,
-            body: JSON.stringify({ uploadUrl: url, imageKey: fileKey }),
+            body: JSON.stringify({ uploadUrl: url, imageId }),
         };
     } catch (err) {
         console.error(err);
