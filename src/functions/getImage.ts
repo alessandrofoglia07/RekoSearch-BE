3ad4a558896aaf7c18bbff68dd438829eb4185fa
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2025-02-04 21:37:42 +0100

    Implement getImage lambda

--- src/functions/getImage.ts
+++ src/functions/getImage.ts
@@ -1,8 +1,27 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, GetCommand } from "@aws-sdk/lib-dynamodb";
+
+const ddb = new DynamoDBClient({ region: process.env.SERVERLESS_AWS_REGION });
+const ddbDocClient = DynamoDBDocumentClient.from(ddb);
+
 export const handler = async (event, context) => {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: 'Hello World',
-        })
-    };
+    try {
+        const { id } = event.pathParameters;
+
+        const { Item } = await ddbDocClient.send(new GetCommand({
+            TableName: process.env.IMAGES_TABLE_NAME,
+            Key: { id }
+        }));
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(Item)
+        };
+    } catch (err) {
+        console.error(err);
+        return {
+            statusCode: 500,
+            body: JSON.stringify(err)
+        };
+    }
 };
