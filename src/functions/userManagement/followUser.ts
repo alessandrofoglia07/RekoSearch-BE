03e26424d974707222a0656c9e0c91e72e409baf
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2025-03-18 22:47:46 +0100

    Change followUser function to also accept username as user identifier

--- src/functions/userManagement/followUser.ts
+++ src/functions/userManagement/followUser.ts
@@ -1,14 +1,41 @@
+import { AdminGetUserCommand, CognitoIdentityProviderClient } from "@aws-sdk/client-cognito-identity-provider";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, GetCommand, UpdateCommand } from "@aws-sdk/lib-dynamodb";
 import { APIGatewayProxyEvent, Handler } from "aws-lambda";
 
 const ddb = new DynamoDBClient({ region: process.env.SERVERLESS_AWS_REGION });
 const ddbDocClient = DynamoDBDocumentClient.from(ddb);
+const cognito = new CognitoIdentityProviderClient({ region: process.env.SERVERLESS_AWS_REGION });
 
 export const handler: Handler = async (event: APIGatewayProxyEvent) => {
     try {
-        const { id } = event.pathParameters ?? {};
+        const { id_or_username } = event.pathParameters ?? {};
         const { sub: userId } = event.requestContext.authorizer!.jwt.claims;
+        const queryType = event.queryStringParameters?.queryType ?? "id";
+
+        if (queryType !== "id" && queryType !== "username") {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({ message: "Bad Request" }),
+            };
+        } else if (!id_or_username) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({ message: "Bad Request" }),
+            };
+        }
+
+        let id: string;
+        if (queryType === 'username') {
+            const user = await cognito.send(new AdminGetUserCommand({
+                UserPoolId: process.env.COGNITO_USER_POOL,
+                Username: id_or_username
+            }));
+            const sub = (user.UserAttributes ?? []).find(attr => attr.Name === "sub")?.Value;
+            sub ? id = sub : id = id_or_username;
+        } else {
+            id = id_or_username;
+        }
 
         const { Item: userToFollow } = await ddbDocClient.send(new GetCommand({
             TableName: process.env.USERS_TABLE_NAME,
